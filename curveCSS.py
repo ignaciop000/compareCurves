f941fcbafa8e19b59b1197265c1892340e20b73e
Nacho <ignaciop000@gmail.com>
2018-12-01 11:10:51 -0300

    avances

--- curveCSS.py
+++ curveCSS.py
@@ -6,7 +6,7 @@ import cv2
 1st and 2nd derivative of 1D gaussian 
 """
 def getGaussianDerivs(sigma, M):
-	L = int((M - 1) / 2);
+	L = int((M - 1) / 2)
 	sigma_sq = sigma * sigma
 	sigma_quad = sigma_sq * sigma_sq
 	gaussian = np.zeros(M)
@@ -43,14 +43,14 @@ def getdX(curveX, n, sigma, gaussian, gaussian1D, gaussian2D, isOpen):
 		if (n-k < 0):
 			if isOpen:
 				#open curve - mirror values on border
-				x_n_k = curveX[-(n-k)]; 
+				x_n_k = curveX[-(n-k)]
 			else:
 				#closed curve - take values from end of curve
 				x_n_k = curveX[len(curveX)+(n-k)]
 		elif n-k > len(curveX)-1:
 			if isOpen:
 				#mirror value on border
-				x_n_k = curveX[n+k]; 
+				x_n_k = curveX[n+k] 
 			else:
 				x_n_k = curveX[(n-k)-(len(curveX))]
 		else:
@@ -73,7 +73,7 @@ def getdXcurve(curveX, sigma, gaussian, gaussian1D, gaussian2D, isOpen):
 		gx[i], dx[i], d2x[i] = getdX(curveX,i,sigma,gaussian,gaussian1D,gaussian2D,isOpen)
 	return (gx, dx, d2x)
 
-def resampleCurve(curveX, curveY, N, isOpen):
+def resampleCurveXY(curveX, curveY, N, isOpen):
 	resamplepl = np.empty(N, dtype=object)
 	resamplepl[0] = (curveX[0], curveY[0])
 	pl = polyLineMerge(curveX,curveY)
@@ -86,7 +86,7 @@ def resampleCurve(curveX, curveY, N, isOpen):
 	i = 1
 	while i<N:       
 		last_dist = cv2.norm(pl[curr] - pl[curr+1])
-		dist += last_dist;
+		dist += last_dist
 		if dist >= resample_size:
 			#put a point on line
 			_d = last_dist - (dist-resample_size)
@@ -103,44 +103,54 @@ def resampleCurve(curveX, curveY, N, isOpen):
 			#if remaining dist to next point needs more sampling... (within some epsilon)
 			while dist - resample_size > 1e-3:
 				resamplepl[i] = resamplepl[i-1] + dirv * resample_size
-				dist -= resample_size;
+				dist -= resample_size
 				i += 1
 		curr+=1    
 	 
 	(resampleX,resampleY) = polyLineSplit(resamplepl)
 	return (resampleX, resampleY)
 
+def resampleCurve( curve, N, isOpen = False):	
+	curveX, curveY = polyLineSplit(curve)
+	resampleX,resampleY = resampleCurveXY(curveX,curveY,N,isOpen)
+	curveResample = polyLineMerge(resampleX, resampleY)
+	return curveResample
 
 """
 	compute curvature of curve after gaussian smoothing 
 	from "Shape similarity retrieval under affine transforms", Mokhtarian & Abbasi 2002
- 	curvex - x position of points
- 	curvey - y position of points
- 	kappa - curvature coeff for each point
- 	sigma - gaussian sigma
+	curvex - x position of points
+	curvey - y position of points
+	kappa - curvature coeff for each point
+	sigma - gaussian sigma
 """
 def computeCurveCSS(curveX, curveY, sigma, isOpen):
-    M = round((10.0*sigma+1.0) / 2.0) * 2 - 1
+	M = round((10.0*sigma+1.0) / 2.0) * 2 - 1
  
-    g,dg,d2g = getGaussianDerivs(sigma,M)
+	g,dg,d2g = getGaussianDerivs(sigma,M)
 
-    smoothX,X,XX = getdXcurve(curveX,sigma,g,dg,d2g, isOpen);
+	smoothX,X,XX = getdXcurve(curveX,sigma,g,dg,d2g, isOpen);
 	smoothY,Y,YY = getdXcurve(curveY,sigma,g,dg,d2g, isOpen);
-         
-    kappa = np.zeros(len(curvex))    
-    for i in range(0, len(curvex));
-        # Mokhtarian 02' eqn (4)
-        kappa[i] = (X[i]*YY[i] - XX[i]*Y[i]) / pow(X[i]*X[i] + Y[i]*Y[i], 1.5);
-    return kappa
+		 
+	kappa = np.zeros(len(curvex))    
+	for i in range(0, len(curvex)):
+		# Mokhtarian 02' eqn (4)
+		kappa[i] = (X[i]*YY[i] - XX[i]*Y[i]) / pow(X[i]*X[i] + Y[i]*Y[i], 1.5)
+	return kappa
+
 """
 	find zero crossings on curvature
 """
 def findCSSInterestPoints(kappa):
-    crossings = []
-    for i in range(0, len(kappa)-1):
-        if ((kappa[i] < 0 && kappa[i+1] > 0) || kappa[i] > 0 && kappa[i+1] < 0):
-            crossings.append(i)
-    return crossings
+	crossings = []
+	for i in range(0, len(kappa)-1):
+		if ((kappa[i] < 0 and kappa[i+1] > 0) or kappa[i] > 0 and kappa[i+1] < 0):
+			crossings.append(i)
+	return crossings
+
+def convertCurve(curve):
+	output = [(i[0],i[1]) for i in curve]	
+	return output
 
 if __name__ == "__main__":
 	x = np.linspace(1, 50, 500)
@@ -154,7 +164,7 @@ if __name__ == "__main__":
 	smoothX,X,XX = getdXcurve(curveX,sigma,gaussian,gaussian1D,gaussian2D, True);
 	smoothY,Y,YY = getdXcurve(curveY,sigma,gaussian,gaussian1D,gaussian2D, True);
 	smooth = polyLineMerge(smoothX,smoothY);
-	resampleX, resampleY = resampleCurve(curveX, curveY, 50, True)
+	resampleX, resampleY = resampleCurveXY(curveX, curveY, 50, True)
 	pl.plot(curveX, curveY)
 	pl.plot(smoothX, smoothY)
 	pl.plot(resampleX, resampleY)
--- curveSignature.py
+++ curveSignature.py
@@ -1,73 +1,79 @@
-def getCurveForImage(image):	
+import cv2
+import numpy as np
+import curveCSS
+import matplotlib.pyplot as pl
+
+def getAllCurveForImage(image):
 	tmp = image.copy()
-	if (tmp.dtype == cv2.CV_8UC3):
+	if (tmp.dtype == np.uint8):
 		gray = cv2.cvtColor(tmp, cv2.COLOR_BGR2GRAY)
 	elif (tmp.dtype == cv2.CV_8UC1):
 		gray = tmp
 	else:
 		raise Exception('Unsupported image format')
 
-	gray = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)
-
-	#cv2.imshow("input",gray);
+	_,gray = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)
+	
+	#cv2.imshow("input",gray)	
 	
 	(_, contours, _) = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 	if len(contours) <= 0:
-		return
-
-	upperCurve = contours[0]
+		return	
+	upperCurve = [(i[0][0],i[0][1]) for i in contours[0]]
 	if (len(upperCurve) <= 50):
 		return
 	
 	#find minimal and maximal X coord
-	x, y = polyLineSplit(contours[0])
-	Point minxp,maxxp;
+	x, y = curveCSS.polyLineSplit(upperCurve)
 	_, _, minxp, maxxp = cv2.minMaxLoc(x)
-	minx = minxp[0]
-	maxx = maxxp[0]
+	minx = minxp[1]
+	maxx = maxxp[1]
+
 	if minx > maxx:
 		minx, maxx = maxx, minx
 	
 	#take lower and upper halves of the curve
-	vector<Point> upper,lower;
-	upper.insert(upper.begin(),contours[0].begin()+minx,contours[0].begin()+maxx);
-	lower.insert(lower.begin(),contours[0].begin()+maxx,contours[0].end());
-	lower.insert(lower.end(),contours[0].begin(),contours[0].begin()+minx);
-	
-	#test which is really the upper part, by looking at the y-coord of the mid point
-	
-	if (lower[lower.size()/2].y <= upper[upper.size()/2].y) {
-		curve_upper = lower;
-		curve_lower = upper;
-	} else {
-		curve_upper = upper;
-		curve_lower = lower;
-	}
+	upper = upperCurve[minx:maxx]
+	lower = upperCurve[maxx:] + upperCurve[:minx]
+
+	#test which is really the upper part, by looking at the y-coord of the mid point	
+	if lower[len(lower)/2][1] <= upper[len(upper)/2][1]:
+		curve_upper = lower
+		curve_lower = upper
+	else:
+		curve_upper = upper
+		curve_lower = lower
 	
 	#make sure it goes left-to-right
-	if (curve_upper.front().x > curve_upper.back().x) { //hmmm, need to flip
-		reverse(curve_upper.begin(), curve_upper.end());
-	}		
+	if (curve_upper[0][0] > curve_upper[-1][0]): #hmmm, need to flip		
+		curve_upper = curve_upper[::-1]
 	
-	whole.clear();
-	whole.insert(whole.begin(),curve_upper.rbegin(),curve_upper.rend());
-	whole.insert(whole.begin(),curve_lower.begin(),curve_lower.end());
+	whole = curve_upper[::-1] + curve_lower[:]
 	return (whole, curve_upper, curve_lower)
 
-def getCurveForImage(image, onlyUpper, getLower):
-	whole,upper,lower = getCurveForImage(image);
+def getCurveForImage(image, onlyUpper, getLower = False):
+	whole,upper,lower = getAllCurveForImage(image);
 	if onlyUpper:
 		if getLower:
 			curve = lower
-		else
+		else:
 			curve = upper
-	else
+	else:
 		curve = whole
 	return curve
 
 if __name__ == "__main__":	
-	#pl.plot(curveX, curveY)
-	#pl.plot(smoothX, smoothY)
-	#pl.plot(resampleX, resampleY)
+	image = cv2.imread("camel-curve.png")
+	#whole, curve_upper, curve_lower = getAllCurveForImage(image)
+	#pl.plot(*zip(*whole))
+	#pl.plot(*zip(*curve_upper))
+	#pl.plot(*zip(*curve_lower))
+	curve = getCurveForImage(image, False)
+	pl.plot(*zip(*curve))
+	curveResample = curveCSS.resampleCurve(curve, 200, False);
+	pl.plot(*zip(*curveResample))
+	a_p2d = curveCSS.convertCurve(curveResample)
+	
+	
 	pl.show()
 
